4049e5da1f96c58e209f98ddfb608774ec2213c1
CodingOdysseyOrginal <139228130+CodingOdysseyOrginal@users.noreply.github.com>
2024-01-08 09:20:15 +0000

    Update script.js

--- script.js
+++ script.js
@@ -23,5 +23,15 @@ button2.onclick = goCave;
 button3.onclick = fightDragon;
 
 function goStore() {
+  button1.innerText = "Buy 10 health (10 gold)";
+  button2.innerText = "Buy weapon (30 gold)"
+  button3.innerText = "Go to town square"
+}
 
+function goCave() {
+  console.log("Going to cave.");
+}
+
+function fightDragon() {
+  console.log("Fighting dragon.")
 }
